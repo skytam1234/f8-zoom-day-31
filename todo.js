795a0a623869cc5ebb32209a7e1e3d7fce852230
skytam123 <skytam1234@gmail.com>
2025-07-14 00:04:57 +0700

    chua xong

--- todo.js
+++ todo.js
@@ -9,6 +9,13 @@ const todoTask = {
   sortCompleteTaskBtn: $(`#completed-btn`),
   modal: $(`.modal`),
   taskTitle: $(`#taskTitle`),
+  taskDescription: $(`#taskDescription`),
+  taskCategory: $(`#taskCategory`),
+  taskPriority: $(`#taskPriority`),
+  startTime: $(`#startTime`),
+  endTime: $(`#endTime`),
+  taskDate: $(`#taskDate`),
+  taskColor: $(`#taskColor`),
   addOrEditForm: $(`.todo-app-form`),
   submitModalBtn: $(`.btn-submit`),
   cancelModalBtn: $(`.btn-cancel`),
@@ -73,6 +80,16 @@ const todoTask = {
   `;
     document.body.appendChild(toastElement);
   },
+  addCurrentTask() {
+    this.taskTitle.value = this.task.title;
+    this.taskDescription.value = this.task.description;
+    this.taskCategory.value = this.task.category;
+    this.taskPriority.value = this.task.priority;
+    this.startTime.value = this.task.startTime;
+    this.endTime.value = this.task.endTime;
+    this.taskDate.value = this.task.dueDate;
+    this.taskColor.value = this.task.color;
+  },
   async send(url, method) {
     const res = await fetch(url, method);
     if (!res.ok) throw new Error(`HTTP code:${(await res).status}`);
@@ -91,10 +108,7 @@ const todoTask = {
     });
   },
   async getTask(id) {
-    console.log(id);
-
     this.task = await this.send(`http://localhost:3000/tasks/` + id);
-    console.log(this.task);
   },
   createTask(newTask) {
     const methods = {
@@ -106,12 +120,29 @@ const todoTask = {
     };
     const respond = this.send("http://localhost:3000/tasks", methods);
   },
-  editTask(id) {
-    this.getTask(id);
+  editTask(id, newTask) {
+    const methods = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    };
+    this.send(`http://localhost:3000/tasks/` + id, methods).then((res) => {
+      this.task = res;
+    });
   },
-  deleteTask(id) {},
+  deleteTask(id) {
+    const methods = { method: "DELETE" };
+    Promise.all([this.send(`http://localhost:3000/tasks/` + id, methods)]).then(
+      () => {
+        this.renderTask();
+      }
+    );
+  },
   async renderTask() {
     await this.getTasks.call(this);
+    console.log(this.tasks);
     let html =
       "" +
       this.tasks
@@ -119,7 +150,7 @@ const todoTask = {
           return `
         <div class="task-card ${this.escape(
           task.color
-        )} ${task.isCompleted ? "completed" : ""}">
+        )} ${task.isCompleted ? "completed" : ""}" data-id=${this.escape(task.id)}>
         <div class="task-header">
           <h3 class="task-title">${this.escape(task.title)}</h3>
           <button class="task-menu">
@@ -129,13 +160,11 @@ const todoTask = {
                 <i class="fa-solid fa-pen-to-square fa-icon"></i>
                 Edit
               </div>
-              <div class="dropdown-item complete-btn" data-index="${task.id}">
+              <div class="dropdown-item complete-btn" data-id="${task.id}">
                 <i class="fa-solid fa-check fa-icon"></i>
                 ${task.isCompleted ? "Mark as Active" : "Mark as Complete"} 
               </div>
-              <div class="dropdown-item delete delete-btn" data-index="${
-                task.id
-              }">
+              <div class="dropdown-item delete delete-btn" data-id="${task.id}">
                 <i class="fa-solid fa-trash fa-icon"></i>
                 Delete
               </div>
@@ -186,7 +215,6 @@ const todoTask = {
     divElement.textContent = html;
     return divElement.innerHTML;
   },
-
   hangDleEvent() {
     this.btnOpenOrCloseForm.onclick = () => {
       this.isInsert = true;
@@ -209,16 +237,70 @@ const todoTask = {
     };
     this.addOrEditForm.onsubmit = (e) => {
       e.preventDefault();
-      const newTask = Object.fromEntries(new FormData(this.addOrEditForm));
-      if (!this.tasks.find((task) => task.title === newTask.title)) {
-        this.createTask.call(this, newTask);
+      if (this.isInsert) {
+        const newTask = Object.fromEntries(new FormData(this.addOrEditForm));
+        if (!this.tasks.find((task) => task.title === newTask.title)) {
+          this.createTask.call(this, newTask);
+          this.createToast(
+            this.toasts[0],
+            "Thành công rồi bạn nhé! Yên tâm đi"
+          );
+          const div = document.createElement("div");
+          div.classList.add(`task-card`);
+          div.classList.add(this.escape(newTask.color));
+          const html = `
+        <div class="task-header">
+          <h3 class="task-title">${this.escape(newTask.title)}</h3>
+          <button class="task-menu">
+            <i class="fa-solid fa-ellipsis fa-icon"></i>
+            <div class="dropdown-menu">
+              <div class="dropdown-item edit-btn" data-id="${newTask.id}">
+                <i class="fa-solid fa-pen-to-square fa-icon"></i>
+                Edit
+              </div>
+              <div class="dropdown-item complete-btn" data-index="${
+                newTask.id
+              }">
+                <i class="fa-solid fa-check fa-icon"></i>
+                ${newTask.isCompleted ? "Mark as Active" : "Mark as Complete"} 
+              </div>
+              <div class="dropdown-item delete delete-btn" data-index="${
+                newTask.id
+              }">
+                <i class="fa-solid fa-trash fa-icon"></i>
+                Delete
+              </div>
+            </div>
+          </button>
+        </div>
+        <p class="task-description">${this.escape(newTask.description)}</p>
+        <div class="task-time">${this.escape(
+          newTask.startTime
+        )} - ${this.escape(newTask.endTime)}</div>
+      `;
+          div.innerHTML = html;
+          this.listTask.appendChild(div);
+          this.toggleTask();
+        } else {
+          this.createToast(
+            this.toasts[3],
+            "Không thể thêm được đâu, trùng title mất rồi bạn ơi."
+          );
+        }
+      } else {
+        const newTask = Object.fromEntries(new FormData(this.addOrEditForm));
+        this.editTask.call(this, this.task.id, newTask);
+        const oldTask = this.listTask.querySelector(
+          `.task-card[data-id="${this.task.id}"]`
+        );
+        this.listTask.removeChild(oldTask);
         this.createToast(this.toasts[0], "Thành công rồi bạn nhé! Yên tâm đi");
         const div = document.createElement("div");
         div.classList.add(`task-card`);
-        div.classList.add(escape(newTask.color));
+        div.classList.add(this.escape(newTask.color));
         const html = `
         <div class="task-header">
-          <h3 class="task-title">${escape(newTask.title)}</h3>
+          <h3 class="task-title">${this.escape(newTask.title)}</h3>
           <button class="task-menu">
             <i class="fa-solid fa-ellipsis fa-icon"></i>
             <div class="dropdown-menu">
@@ -249,11 +331,6 @@ const todoTask = {
         div.innerHTML = html;
         this.listTask.appendChild(div);
         this.toggleTask();
-      } else {
-        this.createToast(
-          this.toasts[3],
-          "Không thể thêm được đâu, trùng title mất rồi bạn ơi."
-        );
       }
     };
     this.listTask.onclick = (e) => {
@@ -263,16 +340,27 @@ const todoTask = {
       if (editBtn) {
         this.isInsert = false;
         this.renderTitleFormTask.call(this);
-        this.editTask.call(this, editBtn.dataset.id);
-
-        //this.toggleTask.call(this);
+        Promise.all([this.getTask.call(this, editBtn.dataset.id)]).then(() => {
+          this.toggleTask.call(this);
+          this.addCurrentTask.call(this);
+        });
       }
       if (completeOrActiveBtn) {
-        this.start.call(this);
+        const newTask = { isCompleted: true };
+        this.editTask.call(this, completeOrActiveBtn.dataset.id, newTask);
+        this.renderTask.call(this);
       }
       if (deleteBtn) {
+        if (confirm("Bạn chắc chắn muốn xóa?")) {
+          this.deleteTask.call(this, deleteBtn.dataset.id);
+          this.createToast(
+            this.toasts[0],
+            "Thành công rồi bạn nhé! Yên tâm đi"
+          );
+        }
       }
     };
+    this.searchBtn.oninput = () => {};
   },
 };
 todoTask.start();
